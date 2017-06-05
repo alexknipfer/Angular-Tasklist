d33a11987235ae9010e7c43523e2d900fd435ba6
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-06-04 19:25:38 -0500

    add, delete, and update tasks on server

--- routes/tasks.js
+++ routes/tasks.js
@@ -22,4 +22,65 @@ router.get('/task/:id', (req, res, next) => {
   })
 })
 
+//Add a task
+router.post('/task', (req, res, next) => {
+  var task = req.body
+  if (!task.title || task.isDone + '') {
+    res.status(400)
+    res.json({
+      error: 'Bad Data'
+    })
+  } else {
+    db.tasks.save(task, (err, task) => {
+      if (err) {
+        res.send(err)
+      }
+      res.json(task)
+    })
+  }
+})
+
+//Add a task
+router.delete('/task/:id', (req, res, next) => {
+  db.tasks.remove({ _id: mongojs.ObjectId(req.params.id) }, (err, task) => {
+    if (err) {
+      res.send(err)
+    }
+    res.json(task)
+  })
+})
+
+//Update a task
+router.put('/task/:id', (req, res, next) => {
+  var task = req.body
+  var updateTask = {}
+
+  if (task.isDone) {
+    udpateTask.isDone = task.isDone
+  }
+
+  if (task.title) {
+    updateTask.title = task.title
+  }
+
+  if (!updateTask) {
+    res.status(400)
+    res.json({
+      error: 'Bad Data'
+    })
+  } else {
+    db.tasks.update(
+      { _id: mongojs.ObjectId(req.params.id) },
+      udpateTask,
+      {},
+      (err, task) => {
+        if (err) {
+          res.send(err)
+        }
+        res.json(task)
+      }
+    )
+  }
+})
+
 module.exports = router
