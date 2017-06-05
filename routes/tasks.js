34a096af5cf4de069472ef3091d2cce54d9d6341
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-06-04 19:18:14 -0500

    find a unique task

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,12 +4,21 @@ var mongojs = require('mongojs')
 var env = require('../env-config')
 var db = mongojs(env.DB_CONNECTION_STRING, ['tasks'])
 
-router.get('/tasks', function(req, res, next) {
-  db.tasks.find(function(err, tasks) {
+router.get('/tasks', (req, res, next) => {
+  db.tasks.find((err, tasks) => {
     if (err) {
       res.send(err)
     }
     res.json(tasks)
+  })
+})
+
+router.get('/task/:id', (req, res, next) => {
+  db.tasks.findOne({ _id: mongojs.ObjectId(req.params.id) }, (err, task) => {
+    if (err) {
+      res.send(err)
+    }
+    res.json(task)
   })
 })
 
